b1362ee4a871e1c02ed3a94337b772f17ef63cd4
griger.szabolcs2@gmail.com <griger.szabolcs2@gmail.com>
2021-10-17 11:19:14 +0200

    Car Model  created

---
+++ KBHKY2_HFT_2021221.Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBHKY2_HFT_2021221.Models
+{
+    public class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public int BasePrice { get; set; }
+
+    }
+}
