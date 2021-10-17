75c41a9c08a7284dd75b96744e868ffc8b574d3c
griger.szabolcs2@gmail.com <griger.szabolcs2@gmail.com>
2021-10-17 11:25:01 +0200

    Brand Model created

---
+++ KBHKY2_HFT_2021221.Models/Brand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBHKY2_HFT_2021221.Models
+{
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [NotMapped]
+        public ICollection<Car> Cars { get; set; }
+        public Brand()
+        {
+            Cars = new HashSet<Car>();
+        }
+    }
+}
