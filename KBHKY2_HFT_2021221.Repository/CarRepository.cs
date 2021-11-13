a630c1f0a9c1142c261f61897edb4dd4e14e4134
griger.szabolcs2@gmail.com <griger.szabolcs2@gmail.com>
2021-11-13 18:58:05 +0100

    CarRepository was created.

---
+++ KBHKY2_HFT_2021221.Repository/CarRepository.cs
@@ -0,0 +1,49 @@
+﻿using KBHKY2_HFT_2021221.Data;
+using KBHKY2_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBHKY2_HFT_2021221.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        OwnerCarBrandContext db;
+        public CarRepository(OwnerCarBrandContext db)
+        {
+            this.db = db;
+        }
+        public void Create(Car car)
+        {
+            db.Cars.Add(car);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(Read(id));
+            db.SaveChanges();
+        }
+
+        public Car Read(int id)
+        {
+            return db.Cars.FirstOrDefault(car => car.Id == id);
+        }
+
+        public IQueryable<Car> ReadAll()
+        {
+            return db.Cars;
+        }
+
+        public void Update(Car car)
+        {
+            var oldCar = Read(car.Id);
+            oldCar.BasePrice = car.BasePrice;
+            oldCar.Model = car.Model;
+            oldCar.BrandId = car.BrandId;
+            db.SaveChanges();
+        }
+    }
+}
