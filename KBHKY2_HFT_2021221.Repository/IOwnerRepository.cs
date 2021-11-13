4380dc759c64aca3dd50d24c915df4c2f828db52
griger.szabolcs2@gmail.com <griger.szabolcs2@gmail.com>
2021-11-13 18:45:22 +0100

    IOwnerRepository was created.

---
+++ KBHKY2_HFT_2021221.Repository/IOwnerRepository.cs
@@ -0,0 +1,18 @@
+﻿using KBHKY2_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBHKY2_HFT_2021221.Repository
+{
+    public interface IOwnerRepository
+    {
+        void Create(Owner owner);
+        void Delete(int id);
+        Owner Read(int id);
+        IQueryable<Owner> ReadAll();
+        void update(Owner owner);
+    }
+}
