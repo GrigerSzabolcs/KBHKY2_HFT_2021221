0f8d38f4c9f97dbd839e534fab1ca57050d9d8c8
griger.szabolcs2@gmail.com <griger.szabolcs2@gmail.com>
2021-11-13 18:43:06 +0100

    IBrandRepository was created.

--- KBHKY2_HFT_2021221.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace KBHKY2_HFT_2021221.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ KBHKY2_HFT_2021221.Repository/IBrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using KBHKY2_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBHKY2_HFT_2021221.Repository
+{
+    public interface IBrandRepository
+    {
+        void Create(Brand car);
+        void Delete(int id);
+        void Update(Brand brand);
+        Brand Read(int id);
+        IQueryable<Brand> ReadAll();
+    }
+}
