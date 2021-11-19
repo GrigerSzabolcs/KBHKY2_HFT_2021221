bc79e7121f9f633218be4aa1bb206cc285f6d535
griger.szabolcs2@gmail.com <griger.szabolcs2@gmail.com>
2021-11-19 16:51:34 +0100

    NuGet downloaded for the UnitTester.

---
+++ KBHKY2_HFT_2021221.Test/Tester.cs
@@ -0,0 +1,27 @@
+﻿using KBHKY2_HFT_2021221.Logic;
+using KBHKY2_HFT_2021221.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace KBHKY2_HFT_2021221.Test
+{
+    [TestFixture]
+    public class Tester
+    {
+        CarLogic cl;
+        BrandLogic bl;
+        OwnerLogic ol;
+        public Tester()
+        {
+            var mockCarRepo =
+                new Mock<ICarRepository>();
+            var mockBrandRepo =
+                new Mock<IBrandRepository>();
+            var mockOwnerRepo =
+                new Mock<IOwnerRepository>();
+
+            cl = new CarLogic(mockCarRepo.Object,mockBrandRepo.Object,mockOwnerRepo.Object);
+        }
+    }
+}
