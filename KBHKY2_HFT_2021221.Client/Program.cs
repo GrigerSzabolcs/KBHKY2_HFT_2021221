a6f0cbb0b3f9f1dc77f53adeecc91dcae08529ff
griger.szabolcs2@gmail.com <griger.szabolcs2@gmail.com>
2021-11-20 19:22:05 +0100

    RestService class created in the Client

--- KBHKY2_HFT_2021221.Client/Program.cs
+++ KBHKY2_HFT_2021221.Client/Program.cs
@@ -1,8 +1,6 @@
-﻿using KBHKY2_HFT_2021221.Data;
-using KBHKY2_HFT_2021221.Logic;
-using KBHKY2_HFT_2021221.Models;
-using KBHKY2_HFT_2021221.Repository;
+﻿using KBHKY2_HFT_2021221.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace KBHKY2_HFT_2021221.Client
@@ -11,22 +9,34 @@ namespace KBHKY2_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            OwnerCarBrandContext db = new OwnerCarBrandContext();
-            CarRepository carRepo = new CarRepository(db);
-            OwnerRepository ownerRepo = new OwnerRepository(db);
-            BrandRepository brandRepo = new BrandRepository(db);
-            CarLogic carLogic = new CarLogic(carRepo, brandRepo, ownerRepo);
+            //OwnerCarBrandContext db = new OwnerCarBrandContext();
+            //CarRepository carRepo = new CarRepository(db);
+            //OwnerRepository ownerRepo = new OwnerRepository(db);
+            //BrandRepository brandRepo = new BrandRepository(db);
+            //CarLogic carLogic = new CarLogic(carRepo, brandRepo, ownerRepo);
 
-            var t1 = carLogic.ModelNamesWithBrand();
-            var t2 = carLogic.AVGPriceByBrands();
-            
-            var t3 = carLogic.CountCarsByBrand();
-            var t4 = carLogic.SeniorOwners();
-            var t5 = carLogic.ExpensiveCarOwners();
-            var t6 = carLogic.MAXPriceByBrands();
-            
+            //var t1 = carLogic.ModelNamesWithBrand();
+            //var t2 = carLogic.AVGPriceByBrands();
 
-            Console.ReadKey();
+            //var t3 = carLogic.CountCarsByBrand();
+            //var t4 = carLogic.SeniorOwners();
+            //var t5 = carLogic.ExpensiveCarOwners();
+            //var t6 = carLogic.MAXPriceByBrands();
+
+            //Console.ReadKey();
+            System.Threading.Thread.Sleep(8000);
+
+            RestService rest = new RestService("http://localhost:54669");
+
+            rest.Post<Brand>(new Brand
+            {
+                Name = "UjMarka"
+            }, "brand");
+
+            var brand = rest.Get<Brand>("brand");
+
+            var countcarsbybrand = rest.Get<KeyValuePair<string, int>>("stat/countcarsbybrand");
+            
 
         }
     }
